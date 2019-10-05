49814a6b7d0c08807144dd91de88179009502b66
RustyMonky <icantor@fandm.edu>
2019-10-05 17:46:18 -0400

    Implements basic title screen and scene switcher

---
+++ levels/title/title.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	pass # Replace with function body.
+
+# Temporary -- replace with texture button later
+func _on_button_pressed():
+	sceneManager.goto_scene("res://levels/main/main.tscn")
