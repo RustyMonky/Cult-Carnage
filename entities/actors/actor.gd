f6e504feb4cade563ba571f0ed6cbfa1e6a154e3
RustyMonky <icantor@fandm.edu>
2019-10-04 21:03:15 -0400

    Adds inputs and logic for player movement and punching

--- entities/actors/actor.gd
+++ entities/actors/actor.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 
+var direction = Vector2()
 var speed = null
 
 func _ready():
 	set_physics_process(true)
+	set_process_input(true)
+
+func _input(event):
+	pass
 
 func _physics_process(delta):
 	pass
@@ -17,5 +22,5 @@ func fire():
 	pass
 
 # Generic movement logic for actors
-func move():
+func move(delta):
 	pass
--- entities/actors/player/player.gd
+++ entities/actors/player/player.gd
@@ -1,7 +1,35 @@
 extends "res://entities/actors/actor.gd"
 
+onready var punchRaycast = $rayPunch
+
 func _ready():
-	pass # Replace with function body.
+	speed = 16
+
+func _input(event):
+	if event.is_action_pressed("player_attack"):
+		print("Attack!")
+		# Eventually, replace the above with:
+		# - turning on the punching raycast
+		# - detecting enemy collision
+		# - if enemy detected, hit 'em
+		# - re-disable raycast
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
+
+	if Input.is_action_pressed("player_up"):
+		direction.y = -1
+		move(delta)
+	elif Input.is_action_pressed("player_down"):
+		direction.y = 1
+		move(delta)
+	elif Input.is_action_pressed("player_left"):
+		direction.x = -1
+		move(delta)
+	elif Input.is_action_pressed("player_right"):
+		direction.x = 1
+		move(delta)
+
+
+func move(delta):
+	self.move_and_collide(direction * speed * delta)
