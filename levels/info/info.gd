31f03bace0b37ce61ed3e4340035b6259fe4ec2b
RustyMonky <icantor@fandm.edu>
2019-10-06 13:28:34 -0400

    Adds info level between title and main

--- levels/info/info.gd
+++ levels/info/info.gd
@@ -4,4 +4,4 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_play_pressed():
-	sceneManager.goto_scene("res://levels/info/info.tscn")
+	sceneManager.goto_scene("res://levels/main/main.tscn")
