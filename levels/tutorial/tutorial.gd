fe31075dd444fedd80092704b7fab84fd7e64976
RustyMonky <ian.cantor@jazzhr.com>
2020-01-31 18:30:51 -0500

    Adds shake to prisoner speech

--- levels/tutorial/tutorial.gd
+++ levels/tutorial/tutorial.gd
@@ -12,8 +12,9 @@ func _ready():
 
 func _input(event):
 	if (event.is_action_pressed("player_attack")):
-		if textToSpeak[textIndex].length() > 0:
+		if !textTimer.is_stopped():
 			prisonerSpeech.visible_characters = 0
+			textTimer.stop()
 			continuePrisonerText()
 
 func continuePrisonerText():
@@ -27,6 +28,7 @@ func continuePrisonerText():
 	else:
 		textToSpeak = []
 		textIndex = 0
+		prisonerSpeech.set_bbcode("")
 
 # Timer for 0.5 second delay on beginning prisoner speech
 func _on_delayTimer_timeout():
@@ -39,7 +41,7 @@ func _on_delayTimer_timeout():
 		"Aim with your mouse.",
 		"Attack, click buttons, and cycle through text by left clicking.",
 		"Now, get ready, I'm gonna give you an openin'.",
-		"[b]AT LAST, I'M FREEEEE![/b]"
+		"[shake rate=50 level=5][b]AT LAST, I'M FREEEEE![/b][/shake]"
 	]
 	prisonerSpeech.set_bbcode(textToSpeak[textIndex])
 	textTimer.start()
